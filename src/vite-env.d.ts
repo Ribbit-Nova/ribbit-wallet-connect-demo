2e61a6c614f74a95357b5c93fc72c05ede28ebe7
Prakhar Shukla <prakhar234@gmail.com>
2025-08-22 00:52:34 +0200

    change sdk version

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,12 +1,1 @@
 /// <reference types="vite/client" />
-
-declare global {
-  interface Window {
-    ribbit?: {
-      type: 'app' | 'extension';
-      postMessage: (event: string, payload?: unknown) => Promise<unknown>;
-      on?: (event: string, handler: (...args: unknown[]) => void) => void;
-      off?: (event: string, handler: (...args: unknown[]) => void) => void;
-    };
-  }
-}
